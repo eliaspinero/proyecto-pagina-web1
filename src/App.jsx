2ebfcc2db6e2912976357f2e114d6960626d838c
eliaspinero <eliasrpt14@gmail.com>
2024-10-04 12:01:14 -0400

    dwadwa

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Página web básica</h1>
+      <h1>Página web básica!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
